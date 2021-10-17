9d73e6d82c058b7f7ad0b626fdc33745d884b655
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-10-16 18:03:06 -0700

    commit tree 9ba2e5b1dc6eb548ec0aa7b13c46f7c01d21660f

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(recordsArr) {
+    const result = recordsArr.find(item => item.result === "W")
+    if (result === undefined) {
+        return undefined;
+    } else {
+        return result.year;
+    }
+}
 
